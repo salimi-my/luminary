b9dde36b35b3898f7580d52f2df3e391abf8fec3
Mohamad Salimi <contact@salimi.my>
2024-02-12 00:29:15 +0800

    Display posts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Str;
 
 class Post extends Model
 {
@@ -21,6 +22,10 @@ class Post extends Model
         'user_id'
     ];
 
+    protected $casts = [
+        'published_at' => 'datetime'
+    ];
+
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
@@ -29,5 +34,15 @@ class Post extends Model
     public function categories(): BelongsToMany
     {
         return $this->belongsToMany(Category::class);
+    }
+
+    public function excerpt(): string
+    {
+        return Str::words(strip_tags($this->body), 30);
+    }
+
+    public function postDate(): string
+    {
+        return $this->published_at->format('d M Y');
     }
 }
