4a5d905381fee538a67a7c2b6a4087353ae9c614
Mohamad Salimi <contact@salimi.my>
2024-02-12 18:44:16 +0800

    Create post factory & seed

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $title = fake()->realText(50);
+        return [
+            'title' => $title,
+            'slug' => str($title)->slug('-'),
+            'thumbnail' => fake()->imageUrl(1024, 648, 'technology', true),
+            'body' => fake()->realText(5000),
+            'active' => fake()->boolean,
+            'published_at' => fake()->dateTime,
+            'user_id' => 1
+        ];
+    }
+}
