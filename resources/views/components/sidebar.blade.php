36d1a5960271b0c8053648b063c98caccbefe342
Mohamad Salimi <contact@salimi.my>
2024-02-16 23:48:12 +0800

    Add href url

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -18,7 +18,7 @@
         <p class="pb-2">
             {!! \App\Models\TextWidget::getContent('about-us-sidebar') !!}
         </p>
-        <a href="#"
+        <a href="{{ route('about-us') }}"
             class="w-full bg-blue-800 text-white font-bold text-sm uppercase rounded hover:bg-blue-700 flex items-center justify-center px-2 py-3 mt-4">
             Get to know us
         </a>
