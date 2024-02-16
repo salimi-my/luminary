176d69ec812084eaae6378d5489ec858ee025b4b
Mohamad Salimi <contact@salimi.my>
2024-02-16 23:17:43 +0800

    Update about us link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -83,7 +83,8 @@
                 <a href="{{ route('by-category', $category->slug) }}"
                     class="hover:bg-blue-600 hover:text-white rounded py-2 px-4 mx-2">{{ $category->title }}</a>
                 @endforeach
-                <a href="#" class="hover:bg-blue-600 hover:text-white rounded py-2 px-4 mx-2">About Us</a>
+                <a href="{{ route('about-us') }}"
+                    class="hover:bg-blue-600 hover:text-white rounded py-2 px-4 mx-2">About Us</a>
             </div>
         </div>
     </nav>
