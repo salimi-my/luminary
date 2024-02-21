b1dd28963afca537312a2df87ab40b6783ece100
Mohamad Salimi <contact@salimi.my>
2024-02-21 22:39:16 +0800

    Assign admin role for user created in admin filament

--- app/Filament/Resources/UserResource/Pages/CreateUser.php
+++ app/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -5,8 +5,24 @@ namespace App\Filament\Resources\UserResource\Pages;
 use App\Filament\Resources\UserResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class CreateUser extends CreateRecord
 {
     protected static string $resource = UserResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function handleRecordCreation(array $data): Model
+    {
+        /** @var \App\Models\User $user */
+
+        $user = parent::handleRecordCreation($data);
+        $user->assignRole('admin');
+
+        return $user;
+    }
 }
