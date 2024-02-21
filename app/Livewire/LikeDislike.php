1effd01b07029343f98828bc46a53d17d876138b
Mohamad Salimi <contact@salimi.my>
2024-02-21 23:21:10 +0800

    Create likedislike livewire component

---
+++ app/Livewire/LikeDislike.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class LikeDislike extends Component
+{
+    public function render()
+    {
+        return view('livewire.like-dislike');
+    }
+}
