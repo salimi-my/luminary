bb592fa05408d58af3316696f129dcbf7a269b93
Mohamad Salimi <contact@salimi.my>
2024-03-09 22:25:41 +0800

    Update about us page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,14 +1,12 @@
 <x-app-layout meta-title="About Us — Luminary"
   meta-description="Explore our diverse perspectives and thought-provoking content covering a wide range of topics.">
 
-  <section class="w-full flex flex-col items-center px-3">
+  <section class="w-full max-w-7xl mx-auto flex flex-col items-center px-3">
 
     <article class="flex flex-col shadow my-4">
       <!-- Article Image -->
-      <a href="#" class="hover:opacity-75">
-        <img src="/storage/{{ $widget->image }}" alt="{{ $widget->title }}" class="w-full object-cover h-64">
-      </a>
-      <div class="bg-white flex flex-col justify-start p-6">
+      <img src="/storage/{{ $widget->image }}" alt="{{ $widget->title }}" class="w-full aspect-[5/2] object-cover">
+      <div class="bg-white flex flex-col justify-start p-6 post-body">
         <h1 class="text-3xl font-bold hover:text-gray-700 pb-4">
           {{ $widget->title }}
         </h1>
