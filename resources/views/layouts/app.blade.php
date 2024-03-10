fa91139347fd7fc91b17d522df5e9db39a6e1336
Mohamad Salimi <contact@salimi.my>
2024-03-10 17:31:02 +0800

    Add icon into search input field

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -90,9 +90,14 @@
 
                 <div class="flex items-center">
                     <form method="get" action="{{ route('search') }}">
-                        <input name="search" value="{{ request()->get('search') }}"
-                            class="block w-full rounded-md border-0 px-3.5 py-2 t0ext-gray-900 shadow-sm ring-1 ring-inset ring-yellow-300 placeholder:text-gray-400 focus:ring-2 focus:ring-primary focus:ring-inset sm:text-sm sm:leading-6 font-medium"
-                            placeholder="Search..." />
+                        <div class="relative">
+                            <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                                <x-heroicon-o-magnifying-glass class="w-5 h-5 text-gray-500" />
+                            </div>
+                            <input name="search" value="{{ request()->get('search') }}"
+                                class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-yellow-300 placeholder:text-gray-400 focus:ring-2 focus:ring-primary focus:ring-inset sm:text-sm sm:leading-6 font-medium pl-10"
+                                placeholder="Search..." />
+                        </div>
                     </form>
 
                     @auth
