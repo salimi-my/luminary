be3a4d4199c4fc4328c0eec1707299a6c747e0a1
Mohamad Salimi <contact@salimi.my>
2024-03-11 00:21:33 +0800

    Update profile page layout

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,26 +1,22 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
+    <div class="py-12 w-full max-md:px-3">
+        <h1 class="w-full max-w-7xl mx-auto text-3xl font-bold sm:px-6 lg:px-8 mb-4">Your Profile</h1>
+        <div class="w-full max-w-7xl grid lg:grid-cols-2 mx-auto sm:px-6 lg:px-8 gap-4">
+            <div class="p-4 sm:p-8 bg-white shadow rounded-lg">
+                <div class="w-full">
                     @include('profile.partials.update-profile-information-form')
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
+            <div class="p-4 sm:p-8 bg-white shadow rounded-lg">
+                <div class="w-full">
                     @include('profile.partials.update-password-form')
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
+            <div class="p-4 sm:p-8 bg-white shadow rounded-lg">
+                <div class="w-full">
                     @include('profile.partials.delete-user-form')
                 </div>
             </div>
