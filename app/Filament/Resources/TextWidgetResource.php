44d2f5ec5d853c99d9a1727cf39ed696841c0ad7
Mohamad Salimi <contact@salimi.my>
2024-03-11 00:47:29 +0800

    Update textwidget menu icon

--- app/Filament/Resources/TextWidgetResource.php
+++ app/Filament/Resources/TextWidgetResource.php
@@ -18,7 +18,7 @@ class TextWidgetResource extends Resource
 {
     protected static ?string $model = TextWidget::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-m-square-3-stack-3d';
 
     protected static ?string $navigationGroup = 'Content';
 
