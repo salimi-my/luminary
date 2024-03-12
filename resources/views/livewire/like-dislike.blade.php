edf49564c1139fd9ba11dc0e30249e817d095c1c
Mohamad Salimi <contact@salimi.my>
2024-03-12 12:58:17 +0800

    Update like dislike component

--- resources/views/livewire/like-dislike.blade.php
+++ resources/views/livewire/like-dislike.blade.php
@@ -1,12 +1,20 @@
 <div class="bg-white px-6 py-2 flex items-center gap-6 shadow">
     <button wire:click="likeDislike(true)"
-        class="flex items-center gap-2 text-green-300 hover:text-yellow-500 transition-all {{ $hasLikeDislike ? 'text-yellow-500' : '' }}">
+        class="flex items-center gap-2 hover:text-green-400 transition-all {{ $hasLikeDislike ? 'opacity-100 text-green-400' : 'opacity-50 text-yellow-600' }}">
+        @if($hasLikeDislike)
+        <x-heroicon-s-hand-thumb-up class="w-5 h-5" />
+        @else
         <x-heroicon-o-hand-thumb-up class="w-5 h-5" />
+        @endif
         {{ $likes }}
     </button>
     <button wire:click="likeDislike(false)"
-        class="flex items-center gap-2 text-red-300 hover:text-yellow-500 transition-all {{ $hasLikeDislike === false ? 'text-yellow-500' : '' }}">
+        class="flex items-center gap-2 hover:text-red-400 transition-all {{ $hasLikeDislike === false ? 'opacity-100 text-red-400' : 'opacity-50 text-yellow-600' }}">
+        @if($hasLikeDislike === false)
+        <x-heroicon-s-hand-thumb-down class="w-5 h-5" />
+        @else
         <x-heroicon-o-hand-thumb-down class="w-5 h-5" />
+        @endif
         {{ $dislikes }}
     </button>
 </div>
